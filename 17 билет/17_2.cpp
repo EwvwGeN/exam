92662fed2cd1850f4979ab41820f575713cc90d4
EwvwGeN <doj54567467z@gmail.com>
2021-12-27 18:06:38 +0300

    power

---
+++ 17 билет/17_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+std::string TransformString(std::string inputString) {
+    char prevSymbol = '0';
+    std::string response = "";
+    for (auto curSymbol : inputString) {
+//        if (prevSymbol == curSymbol) { Тоже самое что и строка внизу только длинее 
+//            response += '0';
+//        } else {
+//            response += '1';
+//        }
+        response += (prevSymbol == curSymbol) ? '0' : '1';
+        prevSymbol = curSymbol;
+    }
+    return response;
+}
+
+int main(int args, char** argv) {
+    std::cout << TransformString("001") << std::endl;
+    std::cout << TransformString("011010") << std::endl;
+    return 0;
+}
