f7494165c23b70e2c7b8d355ed9bf22864fd6092
EwvwGeN <doj54567467z@gmail.com>
2021-12-27 17:46:18 +0300

    edited

--- 10 билет/10.cpp
+++ 10 билет/10.cpp
@@ -5,6 +5,9 @@ using namespace std;
 void function(int a, int b, int c)//создается функция 
 {   
     int buffer = 0;
+    cout << a << " ";
+    cout << b << " ";
+    cout << c << " ";
     for (int n = 0; n < 7; n++)//для отображения первых 10-ти чисел
     {
         buffer = a + b + c;
